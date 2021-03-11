b1973c59ddedc311fb7b014a6c5c4d3a4913454e
DESKTOP-N0219J1\moham <mohammad.sayyed@stud.fh-campuswien.ac.at>
2021-03-11 23:30:33 +0100

    final

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,7 +1,49 @@
+import javax.lang.model.element.NestingKind;
+
 public class MainClass {
+
+
+
     public static void main(String[] args) {
+        String password =new String("111");
+
+        System.out.println(password);
+
 
     }
 
+
+
+    public boolean checkPassword(String password){
+        this.checkLength(password);
+        this.checkAlphanumericSymbols(password);
+        this.checkConsecutiveNumberAndSameN(password);
+        return false;
+
+    }
+    public boolean checkLength(String password){
+        if(password.length()>7 && password.length() < 26){
+            return true;
+        }else {
+            System.out.println("Invalid password length!");
+        }
+        return false;
+    }
+    public boolean checkAlphanumericSymbols(String password){
+        if(password.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$")){
+            return true;
+        }else{
+            System.out.println("Invalid alphanumeric password");
+        }
+        return false;
+    }
+    public boolean checkConsecutiveNumberAndSameN(String password){
+        for (int i = 0; i < password.length(); i++) {
+
+
+        }
+        return false;
+
+    }
 
 }
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -1,2 +1,37 @@
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class MainClassTest {
+    MainClass tester= new MainClass();
+
+
+    @Test
+    public void checckPassword_toSmal(){
+        boolean actual = tester.checkLength("asd");
+        assertTrue(!actual);
+    }
+    @Test
+    public void checkPasswort_alphaNumerich(){
+        boolean actual = tester.checkAlphanumericSymbols("!?/,.?");
+        assertTrue(actual);
+
+    }
+    @Test
+    public void checkPasswordConsecutiveNumber(){
+        boolean actual = tester.checkConsecutiveNumberAndSameN("123");
+        assertFalse(actual);
+    }
+
+    @Test
+    public void checkPasswordTheSame(){
+        boolean actual = tester.checkConsecutiveNumberAndSameN("111");
+        assertFalse(actual);
+    }
+
+
+
+
+
 }
